ec7c7620eb7a3ca8171766eacc3fad241b7b581c
panhao <hins_qt@163.com>
2022-05-19 14:37:00 +0800

    初始化boot项目

---
+++ learn_netty/src/main/java/com/hins/qt/LearnNettyApplication.java
@@ -0,0 +1,17 @@
+package com.hins.qt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ * @author Administrator
+ */
+@SpringBootApplication
+public class LearnNettyApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(LearnNettyApplication.class, args);
+    }
+}
---
+++ learn_netty/src/test/java/com/hins/qt/AppTest.java
@@ -0,0 +1,20 @@
+package com.hins.qt;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
